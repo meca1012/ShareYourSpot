7f313bb3cba4b0cc794ad06d16cfdacddc257369
Constantin Treiber <constantin.treiber@gmail.com>
2013-05-24 01:25:35 +0200

    Addded Some_SampleCode into RestClient

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/restclient/RestClient.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/restclient/RestClient.java
@@ -3,6 +3,7 @@ package de.hska.shareyourspot.android.restclient;
 import de.hska.shareyourspot.android.domain.User;
 
 
+
 public class RestClient extends HttpHandler{
 	public static String BASE_URL = "http://192.168.178.52:8080/ShareYourSpot/rest";
 
@@ -11,4 +12,26 @@ public class RestClient extends HttpHandler{
 		User benutzer = (User) get(uri, DomainType.User);
 		return benutzer;
 	}
-	}
+}	
+		
+//SAMPLES
+		
+		//POST
+			
+		//	public Integer createNewBenutzer(Benutzer benutzer) {
+		//		Integer responseCode = -1;
+		//		String uri = BASE_URL + BENUTZER;
+		//		String xmlStr = serialize(benutzer);
+		//		if (xmlStr != null) {
+		//			responseCode = post(uri, xmlStr);
+		//		}
+		//		return responseCode;
+		//	}
+		
+		//DELETE
+		//	public int deleteTaetikeit(String taetigkeitId) {
+		//		String uri = BASE_URL + TAETIGKEIT + taetigkeitId;
+		//		Integer statusCode = delete(uri);
+		//		return statusCode;
+		//	}
+			
