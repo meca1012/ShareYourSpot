08fec261ca518cb2ebfb0d5a2e1c0e2a30b54dd9
meca1012 <carsten.meiser@googlemail.com>
2013-06-09 17:48:24 +0200

    Navigation ausgebaut

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Friends.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Friends.java
@@ -1,22 +1,40 @@
 package de.hska.shareyourspot.android.activites;
 
+import java.util.List;
+
 import de.hska.shareyourspot.android.R;
+import de.hska.shareyourspot.android.domain.User;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class Friends extends Activity {
-	
+
+	private List<User> friends;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.friends);		
+		setContentView(R.layout.friends);
 	}
-	
+
 	public void showGroups(View view) {
 		Intent intent = new Intent(this, Groups.class);
 		startActivity(intent);
+	}
+
+	public void onClickSearch(View view) {
+
+		// TODO: search my friend
+	}
+
+	public List<User> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(List<User> friends) {
+		this.friends = friends;
 	}
 
 }
--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
@@ -1,33 +1,47 @@
 package de.hska.shareyourspot.android.activites;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import de.hska.shareyourspot.android.R;
+import de.hska.shareyourspot.android.domain.Party;
 
 public class Groups extends Activity {
-	
+
+	private List<Party> groups;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.groups);		
+		setContentView(R.layout.groups);
+
+		// TODO: load groups of the user
 	}
-	
+
 	public void showFriends(View view) {
 		Intent intent = new Intent(this, Friends.class);
 		startActivity(intent);
 	}
-	
-//	public void showMySpots(View view) {
-//		Intent intent = new Intent(this, Spots.class);
-//		startActivity(intent);
-//	}
-	
+
+	// public void showMySpots(View view) {
+	// Intent intent = new Intent(this, Spots.class);
+	// startActivity(intent);
+	// }
+
 	public void newGroup(View view) {
 		Intent intent = new Intent(this, NewGroup.class);
 		startActivity(intent);
 	}
 
+	public List<Party> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<Party> groups) {
+		this.groups = groups;
+	}
 
 }
