1c4151c08e0697e6c9137b60b802ab52bed26aa8
scma1064 <scma1064@hs-karlsruhe.de>
2013-06-10 14:11:52 +0200

    Liste in New Group implementiert zur Freundesauswahl - muss noch mit Freunden befüllt werden

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/NewGroup.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/NewGroup.java
@@ -1,5 +1,52 @@
 package de.hska.shareyourspot.android.activites;
 
-public class NewGroup {
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hska.shareyourspot.android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class NewGroup extends Activity {
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newgroup);
+
+		// TODO: load friends of the user
+		
+		@SuppressWarnings("rawtypes")
+		List valueList = new ArrayList<String>();
+		
+		for (int i = 0; i < 10; i++) {
+		int friend = 0;
+		valueList.add(friend+i);		
+	}
+	
+		@SuppressWarnings({ "rawtypes" })
+		ListAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+		final ListView lv = (ListView)findViewById(R.id.list_FriendsToGroup);
+
+		lv.setAdapter(adapter);
+		
+//		lv.setOnItemClickListener(new OnItemClickListener() {
+//			
+//		@Override
+//		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//		
+//			Intent intent = new Intent();
+//		intent.setClassName(getPackageName(), getPackageName()+”.TutorialListViewActivityAnzeige”);
+//		intent.putExtra(“selected”, lv.getAdapter().getItem(arg2).toString());
+//		startActivity(intent);
+//		}
+//		});
+	}
 
 }
