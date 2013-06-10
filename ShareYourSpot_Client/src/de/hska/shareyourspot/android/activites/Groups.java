75638f6decdde2fef544925cc771f1a6b940315b
scma1064 <scma1064@hs-karlsruhe.de>
2013-06-10 14:18:40 +0200

    Liste hinzugefügt - Gruppen von User müssen dort reingeladen werden

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
@@ -1,24 +1,43 @@
 package de.hska.shareyourspot.android.activites;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import de.hska.shareyourspot.android.R;
 import de.hska.shareyourspot.android.domain.Party;
 
 public class Groups extends Activity {
 
-	private List<Party> groups;
-
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.groups);
 
 		// TODO: load groups of the user
+		
+			
+				@SuppressWarnings("rawtypes")
+				List valueList = new ArrayList<String>();
+				
+				for (int i = 0; i < 10; i++) {
+				int group = 0;
+				valueList.add(group+i);		
+			}
+			
+				@SuppressWarnings({ "rawtypes" })
+				ListAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+				final ListView lv = (ListView)findViewById(R.id.list_groups);
+
+				lv.setAdapter(adapter);
 	}
 
 	public void showFriends(View view) {
@@ -36,12 +55,6 @@ public class Groups extends Activity {
 		startActivity(intent);
 	}
 
-	public List<Party> getGroups() {
-		return groups;
-	}
-
-	public void setGroups(List<Party> groups) {
-		this.groups = groups;
-	}
+	
 
 }
