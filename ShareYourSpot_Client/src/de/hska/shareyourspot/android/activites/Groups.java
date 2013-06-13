5e0d5e6d042268d79d3b38ef05641e4b4a1a3159
meca1012 <carsten.meiser@googlemail.com>
2013-06-13 13:45:05 +0200

    ...

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/Groups.java
@@ -4,41 +4,90 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import de.hska.shareyourspot.android.R;
+import de.hska.shareyourspot.android.domain.Parties;
 import de.hska.shareyourspot.android.domain.Party;
+import de.hska.shareyourspot.android.domain.User;
+import de.hska.shareyourspot.android.restclient.RestClient;
 
 public class Groups extends Activity {
 
+	private RestClient restClient = new RestClient();
+	final Context context = this;
+
+	private Party lookForParty;
+	private List<Party> foundParties;
+	private ListView listGroups;
+	private ArrayList<String> meineListe;
 	
+	public final String groupName = "groupName";
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.groups);
 
-		// TODO: load groups of the user
-		
-			
-				@SuppressWarnings("rawtypes")
-				List valueList = new ArrayList<String>();
-				
-				for (int i = 0; i < 10; i++) {
-				int group = 0;
-				valueList.add(group+i);		
-			}
-			
-				@SuppressWarnings({ "rawtypes" })
-				ListAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
-				final ListView lv = (ListView)findViewById(R.id.list_groups);
-
-				lv.setAdapter(adapter);
+		this.foundParties = new ArrayList<Party>();
+		this.meineListe = new ArrayList<String>();
+		this.listGroups = (ListView) findViewById(R.id.list_groups);
 	}
+
+	public void onClickSearch(View view) {
+
+		this.lookForParty = new Party();
+		EditText editText = (EditText) findViewById(R.id.editText_enterGroupName);
+		this.lookForParty.setName(editText.getText().toString());
+
+		Parties parties = this.restClient.searchParties(this.lookForParty);
+
+		this.foundParties.addAll(parties.getAllParties());
+
+		for (Party party : foundParties) {
+			if (party.getName() != null || party.getName().isEmpty()) {
+				this.meineListe.add(party.getName());
+			}
+		}
+		ListAdapter listenAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, meineListe);
+
+		this.listGroups.setAdapter(listenAdapter);
+		
+		this.listGroups.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				
+				
+				String item = ((TextView) view).getText().toString();
+				
+						Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG)
+								.show();
+						
+//						startGroupDetail(item);
+			}
+		});
+	}
+	
+//	public void startGroupDetail(String name)
+//	{
+//		Intent intent = new Intent(this, Group_Detail.class);
+//		intent.putExtra(groupName, name);
+//		startActivity(intent);
+//	}
 
 	public void showSpots(View view) {
 		Intent intent = new Intent(this, PostList.class);
@@ -50,6 +99,14 @@ public class Groups extends Activity {
 		startActivity(intent);
 	}
 
-	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+
+		String item = ((TextView) v).getText().toString();
+
+		Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+
+		// TODO: joinGroup
+	}
 
 }
