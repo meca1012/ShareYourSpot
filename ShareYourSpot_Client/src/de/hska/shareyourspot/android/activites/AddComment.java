24a0dfa774b11258735aebd6e80472163f69daae
scma1064 <scma1064@hs-karlsruhe.de>
2013-06-25 14:29:31 +0200

    nav/layout

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/AddComment.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/AddComment.java
@@ -15,6 +15,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RatingBar;
@@ -39,6 +40,24 @@ public class AddComment extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.add_comment, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+
+		case R.id.action_logout:
+			uStore.logout(ctx);
+			finish();
+			break;
+
+
+
+		default:
+			break;
+		}
+
 		return true;
 	}
 	
