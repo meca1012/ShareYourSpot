47028b1a12fcce9c2a6817e1509bc68462d08758
Constantin Treiber <constantin.treiber@gmail.com>
2013-06-25 19:25:38 +0200

    nav

--- ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/GoogleMaps.java
+++ ShareYourSpot_Client/src/de/hska/shareyourspot/android/activites/GoogleMaps.java
@@ -4,10 +4,12 @@ import org.apache.http.HttpHost;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import de.hska.shareyourspot.android.R;
 import de.hska.shareyourspot.android.helper.GoogleMapsHelper;
+import de.hska.shareyourspot.android.helper.UserStore;
 
 public class GoogleMaps extends FragmentActivity {
 		
@@ -27,6 +30,8 @@ public class GoogleMaps extends FragmentActivity {
 	  public final String longitude = "longitude";
 	  public final String latitude = "latitude";
 	  private LatLng coordinates;
+	  private UserStore uStore = new UserStore();
+	  private Context ctx = this;
 		
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +59,27 @@ public class GoogleMaps extends FragmentActivity {
 		public boolean onCreateOptionsMenu(Menu menu) {
 			// Inflate the menu; this adds items to the action bar if it is present.
 			getMenuInflater().inflate(R.menu.main, menu);
+			return true;
+		}
+		
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+
+			case R.id.action_logout:
+				uStore.logout(ctx);
+				finish();
+				break;
+
+			case android.R.id.home:
+				onBackPressed();
+				finish();
+				break;
+
+			default:
+				break;
+			}
+
 			return true;
 		}
 
